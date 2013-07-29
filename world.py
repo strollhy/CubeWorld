998e9e76e0b556f6a172b208c9270e25947055c1
strollhy <strollhuyac@gmail.com>
2013-07-29 16:40:55 -0500

    An example

--- world.py
+++ world.py
@@ -1,33 +1,141 @@
 import pyglet
-
 from pyglet.gl import *
+from pyglet import clock, window
 
-fps = pyglet.clock.ClockDisplay()
+'''
+    http://www.learnersdictionary.com/search/aspect
+    a dictionary site
 
-# The game window
-class Window(pyglet.window.Window):
+    http://www.opengl.org/sdk/docs/man2/
+    opengl api reference
+
+'''
+
+def vector(type, *args):
+    '''
+        return a ctype array
+        GLfloat
+        GLuint
+        ...
+    '''
+    return (type*len(args))(*args)
+
+
+
+class model:
+    def __init__(self, vertices, colorMatrix, indice):
+        self.vertices = vertices
+        self.colorMatrix = colorMatrix
+        self.indice = indice
+        self.angle = 0
+
+    def update(self):
+        self.angle += 1
+        self.angle %= 360
+
+    def draw(self):
+        glMatrixMode(GL_MODELVIEW)
+        glLoadIdentity()
+
+        glRotatef(self.angle, 1, 1, 0)
+
+
+        glEnableClientState(GL_VERTEX_ARRAY)
+        glEnableClientState(GL_COLOR_ARRAY)
+
+        glColorPointer(3, GL_FLOAT, 0, vector(GLfloat, *self.colorMatrix))
+        glVertexPointer(3, GL_FLOAT, 0, vector(GLfloat, *self.vertices))
+
+        glDrawElements(GL_QUADS, len(self.indice), GL_UNSIGNED_INT, vector(GLuint, *self.indice))
+
+
+        glDisableClientState(GL_COLOR_ARRAY)
+        glDisableClientState(GL_VERTEX_ARRAY)
+
+
+
+class world:
     def __init__(self):
-        super(Window, self).__init__(fullscreen=False, vsync = False)
-        self.flipScreen = 0
-        glClearColor(1.0, 1.0, 1.0, 1.0)
-        # Run "self.update" 128 frames a second and set FPS limit to 128.
-        pyglet.clock.schedule_interval(self.update, 1.0/128.0)
-        pyglet.clock.set_fps_limit(128)
-
+        self.element = []
 
     def update(self, dt):
-        self.flipScreen = not self.flipScreen
-        pass
+        for obj in self.element:
+            obj.update()
 
-    def on_draw(self):
-        pyglet.clock.tick() # Make sure you tick the clock!
-        if self.flipScreen == 0:
-            glClearColor(0, 1, 0, 1.0)
-        else:
-            glClearColor(1, 0, 0, 1.0)
-        self.clear()
-        fps.draw()
+    def addModel(self, model):
+        self.element.append(model)
 
-# Create a window and run
-win = Window()
+    def draw(self):
+        for obj in self.element:
+            obj.draw()
+
+
+def setup():
+    # look for GL_DEPTH_BUFFER_BIT
+    glEnable(GL_DEPTH_TEST)
+
+
+
+
+
+
+
+win = window.Window(fullscreen=False, vsync=True, resizable=True, height=600, width=600)
+mWorld = world()
+
+cube = (
+    1, 1, 1, #0
+    -1, 1, 1, #1
+    -1, -1, 1, #2
+    1, -1, 1, #3
+    1, 1, -1, #4
+    -1, 1, -1, #5
+    -1, -1, -1, #6
+    1, -1, -1 #7
+)
+
+
+color = (
+    1, 0, 0,
+    1, 0, 0,
+    1, 0, 0,
+    1, 0, 0,
+    0, 1, 0,
+    0, 1, 0,
+    0, 0, 1,
+    0, 0, 1
+)
+
+indice = (
+    0, 1, 2, 3, # front face
+    0, 4, 5, 1, # top face
+    4, 0, 3, 7, # right face
+    1, 5, 6, 2, # left face
+    3, 2, 6, 7 # bottom face
+    #4, 7, 6, 5  #back face
+)
+
+obj = model(cube, color, indice)
+mWorld.addModel(obj)
+
+
+@win.event
+def on_resize(width, height):
+    glViewport(0, 0, width, height)
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    glOrtho(-10, 10, -10, 10, -10, 10)
+    glMatrixMode(GL_MODELVIEW)
+    return pyglet.event.EVENT_HANDLED
+
+@win.event
+def on_draw():
+    glClearColor(0.2, 0.2, 0.2, 0.8)
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    mWorld.draw()
+
+
+pyglet.clock.schedule(mWorld.update)
+clock.set_fps_limit(30)
+setup()
 pyglet.app.run()
