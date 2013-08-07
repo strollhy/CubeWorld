2a951a54cd5ae8a804f835d607288dfa067a32ee
strollhy <strollhuyac@gmail.com>
2013-08-07 14:22:23 -0500

    runnable version

--- world.py
+++ world.py
@@ -1,60 +1,81 @@
-SCREEN_SIZE = (800, 600)
+SCREEN_SIZE = (320, 240)
 
 import pygame
 from pygame.locals import *
-
-from gameobjects.matrix44 import * 
-from gameobjects.vector3 import *
-
-from sys import exit
-from cube import *
-
-
 
 # OpenGL stuff
 from OpenGL.GL import *
 from OpenGL.GLU import *
 
-def resize(width, height):
-    glViewport(0, 0, width, height)
-    glMatrixMode(GL_PROJECTION)
-    glLoadIdentity()
-    gluPerspective(60., float(width)/height, 1., 10000.)
-    glMatrixMode(GL_MODELVIEW)
-    glLoadIdentity()
 
-def init():
-    glEnable(GL_DEPTH_TEST)
-    glClearColor(1.0, 1.0, 1.0, 0.0)
+class myOpenGL:
 
-    glShadeModel(GL_FLAT)
-    glEnable(GL_COLOR_MATERIAL)
+    def __init__(self):
+        # Resize
+        self.resize(*SCREEN_SIZE)
+
+        # OpenGL Setup
+        glEnable(GL_DEPTH_TEST)
+        glClearColor(0.5, 0.69, 1.0, 1)
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        glLoadIdentity()
+
+        # Default parameters
+        self.angle = 0
+
+    def resize(self, width, height):
+        glViewport(0, 0, width, height)
+        glMatrixMode(GL_PROJECTION)
+        glLoadIdentity()
+        gluPerspective(60., float(width)/height, 1., 10000.)
+        glMatrixMode(GL_MODELVIEW)
+        glLoadIdentity()
+
+    def draw(self):
+        # Reset screen 
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        glFlush()
+
+        # Set current matrix
+        glMatrixMode(GL_MODELVIEW)
+
+        # Reset Modelview
+        glLoadIdentity()
+
+        # Change Modelview into screen 5 units
+        glTranslatef(.0, .0, -5.0)
+
+        # Rotate on x, y, z axis
+        self.angle -= 1
+        glRotatef(self.angle, 1, 0, 0)
+
+        
+        # Set "brush" color
+        glColor3f(0.5, 1, 0.5)
+
+        # Begin rendering
+        glBegin(GL_QUADS)
+        glVertex3f(-1, 1, 0)
+        glVertex3f(1, 1, 0)
+        glVertex3f(1, -1, 0)
+        glVertex3f(-1, -1, 0)
+        glEnd()
 
 
-# clock from pygame
-clock = pygame.time.Clock()
 
 ###
 def main():
+    # Initialize pygame
     pygame.init()
     screen = pygame.display.set_mode(SCREEN_SIZE, HWSURFACE|OPENGL|DOUBLEBUF)
     pygame.display.set_caption("Hello, World!")
 
-    resize(*SCREEN_SIZE)
-    init()
+    # clock for framerate
+    clock = pygame.time.Clock()
 
-    # Cube map
-    map = Map()
 
-    # Camera transform matrix
-    camera_matrix = Matrix44()
-    camera_matrix.translate = (10.0, .6, 10.0)
-
-    # Initialize speeds and directions
-    rotation_direction = Vector3()
-    rotation_speed = radians(90.0)
-    movement_direction = Vector3()
-    movement_speed = 5.0
+    # Initialize opengl
+    opengl = myOpenGL()
 
     while True:
         for event in pygame.event.get():
@@ -63,51 +84,10 @@ def main():
             if event.type == KEYUP and event.key == K_ESCAPE:
                 return
 
-        time_passed = clock.tick()
-        time_passed_seconds = time_passed / 1000.
+        clock.tick(50)
 
-        pressed = pygame.key.get_pressed()
-        # Reset rotation and movement directions
-        rotation_direction.set(0.0, 0.0, 0.0)
-        movement_direction.set(0.0, 0.0, 0.0)
-        
-        # Modify direction vectors for key presses
-        if pressed[K_LEFT]:
-            rotation_direction.y = +1.0
-        elif pressed[K_RIGHT]:
-            rotation_direction.y = -1.0
-        if pressed[K_UP]:
-            rotation_direction.x = -1.0
-        elif pressed[K_DOWN]:
-            rotation_direction.x = +1.0
-        if pressed[K_z]:
-            rotation_direction.z = -1.0
-        elif pressed[K_x]:
-            rotation_direction.z = +1.0
-        if pressed[K_q]:
-            movement_direction.z = -1.0
-        elif pressed[K_a]:
-            movement_direction.z = +1.0
+        opengl.draw()
 
-        # Calculate rotation matrix and multiply by camera matrix
-        rotation = rotation_direction * rotation_speed * time_passed_seconds
-        rotation_matrix = Matrix44.xyz_rotation(*rotation)
-        camera_matrix *= rotation_matrix
-
-        # Calculate movement and add it to camera matrix translate
-        heading = Vector3(camera_matrix.forward)
-        movement = heading * movement_direction.z * movement_speed
-        camera_matrix.translate += movement * time_passed_seconds
-        
-        # Upload the inverse camera matrix to OpenGL
-        glLoadMatrixd(camera_matrix.get_inverse().to_opengl())
-        
-        # Light must be transformed as well
-        glLight(GL_LIGHT0, GL_POSITION, (0, 1.5, 1, 0))
-        
-        # Render the map
-        map.render()
-        
         # Show the screen
         pygame.display.flip()
 
